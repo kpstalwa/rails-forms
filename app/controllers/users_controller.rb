9fb8568fb4b3b3df28f59513da1cf352d93cff55
Karam Talwandi <k.talwandi@hotmail.com>
2017-12-06 16:52:09 -0500

    Add edit support and display errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,9 +6,20 @@ class UsersController < ApplicationController
 
 		@user = User.new(user_params)
 		if @user.save
-			redirect_to_new_user_path
+			redirect_to new_user_path
 		else
 			render :new
+		end
+	end
+	def edit 
+		@user = User.find(params[:id])
+	end
+	def update
+		@user = User.find(params[:id])
+		if @user.update(user_params)#form_for hash
+			redirect_to edit_user_path
+		else
+			render :edit
 		end
 	end
 end
